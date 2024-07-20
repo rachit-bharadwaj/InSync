9a45adb85676f00839c7f6d1bd6aa2d00d07b584
Rachit Bharadwaj <mail.rachit.bharadwaj@gmail.com>
2024-07-20 11:58:22 +0530

    chore: initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+// next
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+
+import "./globals.css";
+
+const outfit = Outfit({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "InSync",
+  description:
+    "A social media platform built with Next.js for people to connect.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>{children}</body>
+    </html>
+  );
+}
